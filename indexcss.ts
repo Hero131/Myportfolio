e5e003afcfd220a0a63ee0c810554bdae3b0e27c
Hero131 <139626470+Hero131@users.noreply.github.com>
2024-09-05 11:51:49 +0530

    Add files via upload

---
+++ indexcss.ts
@@ -0,0 +1,16 @@
+.hero {
+    background: url('images/hero-bg.jpg') no-repeat center center;
+    background-size: cover;
+}
+
+.navbar-nav .nav-link {
+    color: #fff;
+}
+
+.navbar-nav .nav-link:hover {
+    color: #f8f9fa;
+}
+
+footer {
+    background-color: #343a40;
+}
